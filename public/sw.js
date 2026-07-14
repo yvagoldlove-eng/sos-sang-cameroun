fac6459a3421a277a121613e6bb3cc522b8e9441
yvagoldlove-eng <yvagoldlove@gmail.com>
2026-07-14 15:46:20 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", () => {
+  // Pas de mise en cache pour l'instant, juste la présence du service worker
+});
